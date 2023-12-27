7561f902dab544804a8c3cc0e4163ad5955d2cf0
samcode-web-hi-you <samratthapa1100@gmail.com>
2023-12-27 03:43:36 -0800

    tailwindcss added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './style.css', './aboutus.html', './programs.html'],
-  theme: {},
+  content: [
+  './index.html',
+  './style.css',
+  './aboutus.html',
+  './programs.html',
+  './ourteam.html',
+  'moremembers.html', ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
+
