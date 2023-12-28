0f96dc32bd3840b1b652cca9836d79eab3748bc1
samcode-web-hi-you <samratthapa1100@gmail.com>
2023-12-27 21:23:51 -0800

    folders added

--- Soch_Pariwartan_Nepal/main/script/practise_script/Main_script/tailwind.config.js
+++ Soch_Pariwartan_Nepal/main/script/practise_script/Main_script/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  './index.html',
-  './style.css',
-  './aboutus.html',
-  './programs.html',
-  './ourteam.html',
+  './**/**/index.html',
+  './**/style.css',
+  './**/**/aboutus.html',
+  './**/**/programs.html',
+  './**/**/ourteam.html',
   'moremembers.html', ],
   theme: {
     extend: {},
